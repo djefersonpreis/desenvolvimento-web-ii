4c1a86246fd336ae400d2cbd084c43d72537c836
Djeferson Preis <djefersonpreis@gmail.com>
2021-05-12 07:16:36 -0300

    Calculadora - Correções finais

--- calculadora/index.php
+++ calculadora/index.php
@@ -1,85 +1,7 @@
 
 <?php
 require('autoload.php');
-
 session_start();
-if(!isset($_SESSION['NUM'])){
-    $_SESSION['NUM'] = 0;
-    $_SESSION['RESULTADO'] = 0;
-    $_SESSION['OPERADOR'] = '';
-}
-if(isset($_POST['num'])){
-    if($_SESSION['NUM'] == 0){
-        $_SESSION['NUM'] = $_POST['num'];
-    } else {
-        $_SESSION['NUM'] = $_SESSION['NUM'] . $_POST['num'];
-    }
-}
-if(isset($_POST['op'])){
-    switch ($_POST['op']){
-        case 'CE':
-            $_SESSION['RESULTADO'] = 0;
-            $_SESSION['OPERADOR'] = '';
-            $_SESSION['NUM'] = 0;
-            break;
-        case '=':
-            realizaCalculo();
-            $_SESSION['NUM'] = $_SESSION['RESULTADO'];
-            $_SESSION['RESULTADO'] = 0;
-            break;
-        case '+':
-            if($_SESSION['OPERADOR'] != ''){
-                realizaCalculo();
-            }
-            $_SESSION['OPERADOR'] = '+';
-            $_SESSION['RESULTADO'] = $_SESSION['NUM'];
-            $_SESSION['NUM'] = 0;
-            break;
-        case '-':
-            if($_SESSION['OPERADOR'] != ''){
-                realizaCalculo();
-            }
-            $_SESSION['OPERADOR'] = '-';
-            $_SESSION['RESULTADO'] = $_SESSION['NUM'];
-            $_SESSION['NUM'] = 0;
-            break;
-        case 'X':
-            if($_SESSION['OPERADOR'] != ''){
-                realizaCalculo();
-            }
-            $_SESSION['OPERADOR'] = 'X';
-            $_SESSION['RESULTADO'] = $_SESSION['NUM'];
-            $_SESSION['NUM'] = 0;
-            break;
-        case '%':
-            if($_SESSION['OPERADOR'] != ''){
-                realizaCalculo();
-            }
-            $_SESSION['OPERADOR'] = '%';
-            $_SESSION['RESULTADO'] = $_SESSION['NUM'];
-            $_SESSION['NUM'] = 0;
-            break;
-    }
-}
-
-function realizaCalculo(){
-    switch ($_SESSION['OPERADOR']){
-        case '+':
-            $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] + $_SESSION['NUM'];
-            break;
-        case '-':
-            $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] - $_SESSION['NUM'];
-            break;
-        case 'X':
-            $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] * $_SESSION['NUM'];
-            break;
-        case '%':
-            $_SESSION['RESULTADO'] = floatval($_SESSION['RESULTADO']) / floatval($_SESSION['NUM']);
-            break;
-    }
-    $_SESSION['OPERADOR'] = '';
-    $_SESSION['NUM'] = 0;
-}
 
 $head = new Head("Calculadora PHP POO");
 $head->addProp(new Meta("viewport", "width=device-width, initial-scale=1"));
@@ -87,7 +9,7 @@ $head->addProp(new Link("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist
 $head->addProp(new Link("./css/style.css", "stylesheet",null, null));
 
 $body = new Body();
-$body->addProp(new MontaCalculadora( ($_SESSION['NUM'] <> 0 ? $_SESSION['NUM'] : $_SESSION['RESULTADO']) ));
+$body->addProp(new MontaCalculadora());
 
 echo (new Html("pt-br", $head, $body));
 
--- calculadora/objects/MontaCalculadora.class.php
+++ calculadora/objects/MontaCalculadora.class.php
@@ -9,9 +9,86 @@ class MontaCalculadora {
 	 * Constructor.
 	 * @param $value
 	 */
-	public function __construct($value = 0) {
-		$this->value = $value;
+	public function __construct() {
+        if(!isset($_SESSION['NUM'])){
+            $_SESSION['NUM'] = 0;
+            $_SESSION['RESULTADO'] = 0;
+            $_SESSION['OPERADOR'] = '';
+        }
+        if(isset($_POST['num'])){
+            if($_SESSION['NUM'] == 0){
+                $_SESSION['NUM'] = $_POST['num'];
+            } else {
+                $_SESSION['NUM'] = $_SESSION['NUM'] . $_POST['num'];
+            }
+        }
+        if(isset($_POST['op'])){
+            switch ($_POST['op']){
+                case 'CE':
+                    $_SESSION['RESULTADO'] = 0;
+                    $_SESSION['OPERADOR'] = '';
+                    $_SESSION['NUM'] = 0;
+                    break;
+                case '=':
+                    $this->realizaCalculo();
+                    $_SESSION['NUM'] = $_SESSION['RESULTADO'];
+                    $_SESSION['RESULTADO'] = 0;
+                    break;
+                case '+':
+                    if($_SESSION['OPERADOR'] != ''){
+                        $this->realizaCalculo();
+                    }
+                    $_SESSION['OPERADOR'] = '+';
+                    $_SESSION['RESULTADO'] = $_SESSION['NUM'];
+                    $_SESSION['NUM'] = 0;
+                    break;
+                case '-':
+                    if($_SESSION['OPERADOR'] != ''){
+                        $this->realizaCalculo();
+                    }
+                    $_SESSION['OPERADOR'] = '-';
+                    $_SESSION['RESULTADO'] = $_SESSION['NUM'];
+                    $_SESSION['NUM'] = 0;
+                    break;
+                case 'X':
+                    if($_SESSION['OPERADOR'] != ''){
+                        $this->realizaCalculo();
+                    }
+                    $_SESSION['OPERADOR'] = 'X';
+                    $_SESSION['RESULTADO'] = $_SESSION['NUM'];
+                    $_SESSION['NUM'] = 0;
+                    break;
+                case '%':
+                    if($_SESSION['OPERADOR'] != ''){
+                        $this->realizaCalculo();
+                    }
+                    $_SESSION['OPERADOR'] = '%';
+                    $_SESSION['RESULTADO'] = $_SESSION['NUM'];
+                    $_SESSION['NUM'] = 0;
+                    break;
+            }
+        }
+        $this->value = ($_SESSION['NUM'] <> 0 ? $_SESSION['NUM'] : $_SESSION['RESULTADO']);
 	}
+
+    private function realizaCalculo(){
+        switch ($_SESSION['OPERADOR']){
+            case '+':
+                $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] + $_SESSION['NUM'];
+                break;
+            case '-':
+                $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] - $_SESSION['NUM'];
+                break;
+            case 'X':
+                $_SESSION['RESULTADO'] = $_SESSION['RESULTADO'] * $_SESSION['NUM'];
+                break;
+            case '%':
+                $_SESSION['RESULTADO'] = floatval($_SESSION['RESULTADO']) / floatval($_SESSION['NUM']);
+                break;
+        }
+        $_SESSION['OPERADOR'] = '';
+        $_SESSION['NUM'] = 0;
+    }
 
 	public function __toString() {
         $html = "<form method='post' class='container calculadora' action='index.php'> \n";
